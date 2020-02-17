f861c9a41158da77ddec6ebf653ebc7a4739b2d5
thorly <leiyuan1229@outlook.com>
2020-02-17 11:43:42 +0800

    完善相关提示

--- 复制单个burst并修改文件名/Copy_Change.py
+++ 复制单个burst并修改文件名/Copy_Change.py
@@ -7,7 +7,7 @@ if hasattr(sys, 'frozen'):
 from PyQt5.QtWidgets import QLabel, QPushButton, QTextEdit, QLineEdit, \
     QGridLayout, QWidget, QApplication, QSizePolicy, QFileDialog
 from PyQt5.QtGui import QIcon, QFont
-from PyQt5.QtCore import QThread, pyqtSignal
+from PyQt5.QtCore import QThread, pyqtSignal, Qt
 import datetime
 import shutil
 import re
@@ -81,11 +81,12 @@ class Window(QWidget):
         self.setWindowTitle('复制单个 burst 到指定文件夹并重命名')
         self.setWindowIcon(QIcon(':/copy.ico'))
         self.setFont(QFont('Consolas'))
-        self.setFixedSize(830, 300)
+        self.resize(850, 340)
         self.thread = CopyThread()
         self.setup_ui()
 
     def setup_ui(self):
+        # 添加控件
         self.label1 = QLabel('burst 信息文件：', self)
         self.label2 = QLabel('保存文件路径：', self)
         self.label3 = QLabel('ENVI 导出文件路径：', self)
@@ -96,14 +97,10 @@ class Window(QWidget):
         self.le_imported_path = QLineEdit(self)
         self.le_imported_path.setReadOnly(True)
 
-        self.label1.setBuddy(self.le_burst_path)
-        self.label2.setBuddy(self.le_saving_path)
-        self.label3.setBuddy(self.le_imported_path)
-
-        self.btn_burst = QPushButton('打开', self)
-        self.btn_saving = QPushButton('打开', self)
-        self.btn_import = QPushButton('打开', self)
-        self.btn_start = QPushButton('开始', self)
+        self.btn_burst = QPushButton('选择文件', self)
+        self.btn_saving = QPushButton('选择路径', self)
+        self.btn_import = QPushButton('选择路径', self)
+        self.btn_start = QPushButton('开始复制', self)
         self.btn_start.setEnabled(False)
         self.btn_start.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Expanding)
 
@@ -112,22 +109,24 @@ class Window(QWidget):
         self.ted_info.setText(
             "@author  : leiyuan \n@version : 2.12\n"
             "@date    : 2020-02-04\n\n"
-            "burst信息文件说明:要求文件中有两列数据(date和burst)，以空格分开，"
-            "110代表burst_IW1_10，以此类推\n"
+            "burst信息文件说明:文件中含有两列数据(date和burst)，每行以空格作为分隔符，"
+            "date表示日期，burst表示相关信息。\n\n"
+            "以下示例数据中，20200102表示影像日期，19代表burst_IW1_9；"
+            "20200103表示影像日期，110代表burst_IW1_10\n\n"
             "20200102 19\n20200103 110")
         # 设置布局
         layout = QGridLayout()
         layout.setSpacing(5)
-        layout.addWidget(self.label1, 0, 1)
+        layout.addWidget(self.label1, 0, 1, Qt.AlignRight)
         layout.addWidget(self.le_burst_path, 0, 2, 1, 3)
         layout.addWidget(self.btn_burst, 0, 5)
         self.btn_burst.setFixedSize(self.btn_burst.sizeHint())
 
-        layout.addWidget(self.label3, 1, 1)
+        layout.addWidget(self.label3, 1, 1, Qt.AlignRight)
         layout.addWidget(self.le_imported_path, 1, 2, 1, 3)
         layout.addWidget(self.btn_import, 1, 5)
 
-        layout.addWidget(self.label2, 2, 1)
+        layout.addWidget(self.label2, 2, 1, Qt.AlignRight)
         layout.addWidget(self.le_saving_path, 2, 2, 1, 3)
         layout.addWidget(self.btn_saving, 2, 5)
 
@@ -150,33 +149,35 @@ class Window(QWidget):
         self.btn_start.clicked.connect(self.start_thread_slot)
 
     def start_thread_slot(self):
+        """开始复制文件"""
         self.thread.burst_path = self.le_burst_path.text()
         self.thread.imported_path = self.le_imported_path.text()
         self.thread.save_path = self.le_saving_path.text()
         self.thread.start()
 
     def set_state_slot(self, le1, le2, le3):
+        """设置开始复制按钮是否可用"""
         if le1.text() and le2.text() and le3.text():
             self.btn_start.setEnabled(True)
         else:
             self.btn_start.setEnabled(False)
 
     def get_burst_slot(self):
+        """打开对话框，选择burst信息文件"""
         file_name = QFileDialog.getOpenFileName(
-            self, '选择burst信息文件', './', 'All files(*.*);;txt file(*.txt)', 'txt file(*.txt)'
-        )
+            self, '选择burst信息文件', './', 'All files(*.*);;txt file(*.txt)', 'txt file(*.txt)')
         self.le_burst_path.setText(file_name[0])
 
     def get_saving_slot(self):
+        """打开对话框，选择保存路径"""
         dir_name = QFileDialog.getExistingDirectory(
-            self, '选择保存路径', './'
-        )
+            self, '选择保存路径', './')
         self.le_saving_path.setText(dir_name)
 
     def get_imported_slot(self):
+        """打开对话框，选择ENVI导出文件路径"""
         dir_name = QFileDialog.getExistingDirectory(
-            self, '选择ENVI导出路径', '../'
-        )
+            self, '选择ENVI导出路径', '../')
         self.le_imported_path.setText(dir_name)
 
 
