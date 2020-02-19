c62388501341659564453028ad2094d8e77347e7
thorly <leiyuan1229@outlook.com>
2020-02-19 13:51:43 +0800

    添加下载ALOS dem功能，并优化代码

--- SRTM90mDEM下载/DownloadSRTM.py
+++ SRTM90mDEM下载/DownloadSRTM.py
@@ -10,6 +10,7 @@ from srtm import srtm_dem_no
 
 class ExecIDMThread(QThread):
     """execute IDM class"""
+
     def __init__(self):
         super(ExecIDMThread, self).__init__()
         self.idm_path = ''
@@ -48,105 +49,113 @@ class IDMThread(QThread):
         self.sin_error_num.emit(self.error_num)
 
 
-class MyTextEdit(QTextEdit):
+class TextEdit(QTextEdit):
     """reload mousePressEvent class"""
+
+    def __init__(self, parent=None, *args, **kwargs):
+        super().__init__(parent, *args, **kwargs)
+
     def mousePressEvent(self, me):
         # 继承父类方法
         super().mousePressEvent(me)
         url = self.anchorAt(me.pos())
-        if url.endswith('.zip'):
+        if url.endswith('.zip') or url.endswith('.gz'):
             QDesktopServices.openUrl(QUrl(url))
 
 
 class Window(QWidget):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle('下载SRTM 90m DEM')
+        self.setWindowTitle('Download DEM')
         self.setWindowIcon(QIcon(':/download.ico'))
-        # self.setWindowFlag(Qt.WindowStaysOnTopHint)
         self.setFont(QFont('Consolas'))
-        self.resize(700, 320)
+        self.resize(800, 400)
         self.exec_thread = ExecIDMThread()
         self.idm_thread = IDMThread()
         self.setup_ui()
 
     def setup_ui(self):
         # 添加控件
-        self.lb_lon_min = QLabel('最小经度：', self)
-        self.lb_lon_max = QLabel('最大经度：', self)
-        self.spin_lon_w = QSpinBox(self)
-        self.spin_lon_w.setMaximum(180)
-        self.spin_lon_w.setMinimum(-180)
+        self.lb_dem = QLabel('DEM类型：')
+        self.radio_srtm = QRadioButton('SRTM 90m (3s)')
+        self.radio_srtm.setChecked(True)
+        self.radio_alos = QRadioButton('ALOS 30m (1s)')
+        self.lb_lon = QLabel('最小、最大经度：')
+        self.spin_lon_w = QSpinBox()
+        self.spin_lon_w.setRange(-180, 180)
+        self.spin_lon_w.setToolTip('最小经度')
         self.spin_lon_w.setSuffix("°")
-        self.spin_lon_w.setValue(-180)
-        self.spin_lon_e = QSpinBox(self)
-        self.spin_lon_e.setMaximum(180)
-        self.spin_lon_e.setMinimum(-180)
+        self.spin_lon_w.setAccelerated(True)
+        self.spin_lon_e = QSpinBox()
+        self.spin_lon_e.setRange(-180, 180)
+        self.spin_lon_e.setToolTip('最大经度')
         self.spin_lon_e.setSuffix("°")
-        self.spin_lon_e.setValue(-100)
+        self.spin_lon_e.setAccelerated(True)
 
-        self.lb_lat_min = QLabel('最小纬度：', self)
-        self.lb_lat_max = QLabel('最大纬度：', self)
-        self.spin_lat_s = QSpinBox(self)
-        self.spin_lat_s.setMaximum(60)
-        self.spin_lat_s.setMinimum(-60)
+        self.lb_lat = QLabel('最小、最大纬度：')
+        self.spin_lat_s = QSpinBox()
+        self.spin_lat_s.setRange(-90, 90)
+        self.spin_lat_s.setToolTip('最小纬度')
         self.spin_lat_s.setSuffix("°")
-        self.spin_lat_s.setValue(50)
-        self.spin_lat_n = QSpinBox(self)
-        self.spin_lat_n.setMaximum(60)
-        self.spin_lat_n.setMinimum(-60)
+        self.spin_lat_s.setAccelerated(True)
+        self.spin_lat_n = QSpinBox()
+        self.spin_lat_n.setRange(-90, 90)
+        self.spin_lat_n.setToolTip('最大纬度')
         self.spin_lat_n.setSuffix("°")
-        self.spin_lat_n.setValue(60)
+        self.spin_lat_n.setAccelerated(True)
 
-        self.btn_url = QPushButton('获取\n下载链接', self)
-        self.btn_url.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Expanding)
+        self.btn_url = QPushButton('获取DEM下载链接')
+        self.btn_url.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
 
-        self.btn_add_idm = QPushButton('启动IDM\n并添加任务', self)
+        self.btn_add_idm = QPushButton('启动IDM\n并添加任务')
         self.btn_add_idm.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Expanding)
         self.btn_add_idm.setEnabled(False)
 
-        self.lb_dir = QLabel('DEM保存路径：', self)
-        self.le_dir = QLineEdit(self)
-        self.btn_dir = QPushButton('选择保存路径', self)
+        self.lb_dir = QLabel('DEM保存路径：')
+        self.le_dir = QLineEdit()
+        self.btn_dir = QPushButton('选择路径')
+        self.btn_dir.setFixedSize(self.btn_dir.sizeHint())
 
-        self.lb_idm = QLabel('IDMan.exe路径：', self)
-        self.le_idm = QLineEdit(self)
-        self.btn_idm = QPushButton('选择IDM路径', self)
+        self.lb_idm = QLabel('IDMan.exe路径：')
+        self.le_idm = QLineEdit()
+        self.btn_idm = QPushButton('选择路径')
 
-        self.ted_info = MyTextEdit(self)
+        self.ted_info = TextEdit()
         self.ted_info.setReadOnly(True)
         self.ted_info.setText(
-            "@author  : leiyuan \n@version : 1.4\n"
-            "@date    : 2020-02-16\n\n"
-            "该工具包含两种下载模式:\n"
-            "1. 利用电脑默认浏览器下载（设置好经纬度范围，点击“获取下载链接”即可）\n"
-            "2. 利用IDM进行下载（获取下载链接后，设置保存路径和IDMan.exe路径，点击“添加到IDM”即可）")
+            "@author  : leiyuan \n@version : 2.0\n"
+            "@date    : 2020-02-19\n\n"
+            "该工具包含两种下载模式:\n\n"
+            "1. 利用电脑默认浏览器下载（设置好经纬度范围，点击<获取下载链接>即可）\n\n"
+            "2. 利用IDM进行下载（获取下载链接后，设置DEM保存路径和IDMan.exe路径，点击<添加到IDM>即可）")
 
         # 布局设置
         layout = QGridLayout()
         self.setLayout(layout)
         # 第一行
-        layout.addWidget(self.lb_lon_min, 0, 0, Qt.AlignRight)
-        layout.addWidget(self.spin_lon_w, 0, 1)
-        layout.addWidget(self.lb_lon_max, 0, 2, Qt.AlignRight)
-        layout.addWidget(self.spin_lon_e, 0, 3)
-        layout.addWidget(self.btn_url, 0, 4, 2, 1)
+        layout.addWidget(self.lb_dem, 0, 0, Qt.AlignRight)
+        layout.addWidget(self.radio_srtm, 0, 1)
+        layout.addWidget(self.radio_alos, 0, 2)
         # 第二行
-        layout.addWidget(self.lb_lat_min, 1, 0, Qt.AlignRight)
-        layout.addWidget(self.spin_lat_s, 1, 1)
-        layout.addWidget(self.lb_lat_max, 1, 2, Qt.AlignRight)
-        layout.addWidget(self.spin_lat_n, 1, 3)
+        layout.addWidget(self.lb_lon, 1, 0, Qt.AlignRight)
+        layout.addWidget(self.spin_lon_w, 1, 1)
+        layout.addWidget(self.spin_lon_e, 1, 2)
+        layout.addWidget(self.btn_url, 1, 3, 2, 2)
         # 第三行
-        layout.addWidget(self.lb_dir, 2, 0, Qt.AlignRight)
-        layout.addWidget(self.le_dir, 2, 1, 1, 2)
-        layout.addWidget(self.btn_dir, 2, 3)
-        layout.addWidget(self.btn_add_idm, 2, 4, 2, 1)
+        layout.addWidget(self.lb_lat, 2, 0, Qt.AlignRight)
+        layout.addWidget(self.spin_lat_s, 2, 1)
+        layout.addWidget(self.spin_lat_n, 2, 2)
         # 第四行
-        layout.addWidget(self.lb_idm, 3, 0, Qt.AlignRight)
-        layout.addWidget(self.le_idm, 3, 1, 1, 2)
-        layout.addWidget(self.btn_idm, 3, 3)
+        layout.addWidget(self.lb_dir, 3, 0, Qt.AlignRight)
+        layout.addWidget(self.le_dir, 3, 1, 1, 2)
+        layout.addWidget(self.btn_dir, 3, 3)
+        layout.addWidget(self.btn_add_idm, 3, 4, 2, 1)
         # 第五行
-        layout.addWidget(self.ted_info, 4, 0, 3, 5)
+        layout.addWidget(self.lb_idm, 4, 0, Qt.AlignRight)
+        layout.addWidget(self.le_idm, 4, 1, 1, 2)
+        layout.addWidget(self.btn_idm, 4, 3)
+        # 第六行
+        layout.addWidget(self.ted_info, 5, 0, 3, 5)
 
         # 信号与槽
         self.btn_url.clicked.connect(self.gen_url_slot)
@@ -159,7 +168,7 @@ class Window(QWidget):
         self.idm_thread.sin_error_num.connect(self.error_num_slot)
 
     def warning_slot(self, info):
-        """没有获取链接而点击添加到IDM时，弹出警告信息"""
+        """弹出警告信息"""
         mb = QMessageBox(QMessageBox.Warning, "Warning", info, QMessageBox.Ok, self)
         mb.show()
 
@@ -167,80 +176,123 @@ class Window(QWidget):
         """是否全部添加到IDM"""
         if error_num == 0 and self.idm_thread.url:
             # 添加一个开始锚点
-            self.ted_info.append(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + ' 所有任务都被添加到IDM')
+            time_now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            self.ted_info.append('{} 所有任务都被添加到IDM'.format(time))
         else:
-            self.ted_info.append(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + '有 ' + str(
-                len(self.idm_thread.url) - error_num) + ' 个任务未被添加到IDM')
+            time_now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            self.ted_info.append('{}有 {} 个任务未被添加到IDM'.format(time_now, len(self.idm_thread.url) - error_num))
 
     def gen_url_slot(self):
         """获取DEM下载链接"""
-        lon_min = -180
-        lat_max = 60
-        interval = 5
-        srtm_url = []
-        url_header = "http://srtm.csi.cgiar.org/wp-content/uploads/files/srtm_5x5/tiff/srtm_"
-        # 计算dem编号
-        lon0 = (self.spin_lon_w.value() - lon_min) / interval + 1
-        lon1 = (self.spin_lon_e.value() - lon_min) / interval + 1
-        lat0 = (lat_max - self.spin_lat_n.value()) / interval + 1
-        lat1 = (lat_max - self.spin_lat_s.value()) / interval + 1
-        if lon0 > int(lon0):
-            lon0 = int(lon0)
-        if lon1 > int(lon1):
-            lon1 = int(lon1 + 1)
-        if lat0 > int(lat0):
-            lat0 = int(lat0)
-        if lat1 > int(lat1):
-            lat1 = int(lat1 + 1)
+        download_url = []
         html = []
         no_srtm = []
-        for i in range(int(lon0), int(lon1)):
-            for j in range(int(lat0), int(lat1)):
-                # 计算dem的经纬度范围
-                lon_w = i * 5 - 180
-                lon_e = lon_w - 5
-                lat_s = 60 - j * 5
-                lat_n = lat_s + 5
-                # 设置格式，补零
-                lon_w = "0" + str(lon_w) if len(str(lon_w)) == 1 else str(lon_w)
-                lon_e = "0" + str(lon_e) if len(str(lon_e)) == 1 else str(lon_e)
-                lat_s = "0" + str(lat_s) if len(str(lat_s)) == 1 else str(lat_s)
-                lat_n = "0" + str(lat_n) if len(str(lat_n)) == 1 else str(lat_n)
-                lon_lat = "(" + lon_e + "° ~ " + lon_w + "° " + lat_s + "° ~ " + lat_n + "°)"
-                num_lon = str(i)
-                num_lat = str(j)
-                if len(num_lon) == 1:
-                    num_lon = "0" + num_lon
-                if len(num_lat) == 1:
-                    num_lat = "0" + num_lat
-                name = "srtm" + "_" + num_lon + "_" + num_lat + ".zip"
-                if name not in srtm_dem_no:
-                    url = url_header + num_lon + "_" + num_lat + ".zip"
-                    srtm_url.append(url)
-                    html.append(lon_lat + " " + "<a href=" + url + ">" + name + "</a>")
+        srtm_url_header = "http://srtm.csi.cgiar.org/wp-content/uploads/files/srtm_5x5/tiff/srtm_"
+        alos_url_header = "https://www.eorc.jaxa.jp/ALOS/aw3d30/data/release_v1903/"
+        if self.spin_lon_w.value() >= self.spin_lon_e.value() or self.spin_lat_s.value() >= self.spin_lat_n.value():
+            self.warning_slot('经纬度范围错误，请重新设置')
+        if self.radio_srtm.isChecked():
+            if self.spin_lat_s.value() < -60 or self.spin_lat_n.value() > 60:
+                self.warning_slot('SRTM dem 纬度范围在-60° ~ 60°之间，请重新选择纬度范围，或者选择ALOS dem')
+            else:
+                # 编号计算起始点经纬度
+                lon_min = -180
+                lat_max = 60
+                # 计算dem编号
+                num_min_lon = (self.spin_lon_w.value() - lon_min) / 5 + 1
+                num_max_lon = (self.spin_lon_e.value() - lon_min) / 5 + 1
+                num_min_lat = (lat_max - self.spin_lat_n.value()) / 5 + 1
+                num_max_lat = (lat_max - self.spin_lat_s.value()) / 5 + 1
+                if num_min_lon > int(num_min_lon):
+                    num_min_lon = int(num_min_lon)
+                if num_max_lon > int(num_max_lon):
+                    num_max_lon = int(num_max_lon + 1)
+                if num_min_lat > int(num_min_lat):
+                    num_min_lat = int(num_min_lat)
+                if num_max_lat > int(num_max_lat):
+                    num_max_lat = int(num_max_lat + 1)
+                for i in range(int(num_min_lon), int(num_max_lon)):
+                    for j in range(int(num_min_lat), int(num_max_lat)):
+                        # 计算dem的经纬度范围
+                        lon_w = i * 5 - 180
+                        lon_e = lon_w - 5
+                        lat_s = 60 - j * 5
+                        lat_n = lat_s + 5
+                        # 设置格式，补零
+                        lon_w = "0" + str(lon_w) if len(str(lon_w)) == 1 else str(lon_w)
+                        lon_e = "0" + str(lon_e) if len(str(lon_e)) == 1 else str(lon_e)
+                        lat_s = "0" + str(lat_s) if len(str(lat_s)) == 1 else str(lat_s)
+                        lat_n = "0" + str(lat_n) if len(str(lat_n)) == 1 else str(lat_n)
+                        lon_lat = "({}° ~ {}° , {}° ~ {}°)".format(lon_e, lon_w, lat_s, lat_n)
+                        num_lon = str(i)
+                        num_lat = str(j)
+                        if len(num_lon) == 1:
+                            num_lon = "0" + num_lon
+                        if len(num_lat) == 1:
+                            num_lat = "0" + num_lat
+                        name = "srtm_{}_{}.zip".format(num_lon, num_lat)
+                        if name not in srtm_dem_no:
+                            url = "{}{}_{}.zip".format(srtm_url_header, num_lon, num_lat)
+                            download_url.append(url)
+                            html.append("{} <a href={}>{}</a>".format(lon_lat, url, name))
+                        else:
+                            no_srtm.append("{} 此范围内无DEM".format(lon_lat))
+        else:
+            import math
+            lon_min = (self.spin_lon_w.value()) // 5 * 5
+            lon_max = math.ceil((self.spin_lon_e.value()) / 5) * 5
+            lat_min = (self.spin_lat_s.value()) // 5 * 5
+            lat_max = math.ceil((self.spin_lat_n.value()) / 5) * 5
+
+            def format_num(num, flag):
+                if num < 0:
+                    zero_num = flag - len(str(num)[1:])
+                    if zero_num > 0:
+                        return "0" * zero_num + str(num)[1:]
+                    else:
+                        return str(num)[1:]
                 else:
-                    no_srtm.append(lon_lat + " 此范围内无DEM")
+                    zero_num = flag - len(str(num))
+                    if zero_num > 0:
+                        return "0" * zero_num + str(num)
+                    else:
+                        return str(num)
+
+            for i in range(lat_min, lat_max, 5):
+                for j in range(lon_min, lon_max, 5):
+                    i_5, j_5 = i + 5, j + 5
+                    i_0 = "S{}".format(format_num(i, 3)) if i < 0 else "N{}".format(format_num(i, 3))
+                    j_0 = "W{}".format(format_num(j, 3)) if j < 0 else "E{}".format(format_num(j, 3))
+                    i_5 = "S{}".format(format_num(i_5, 3)) if i_5 < 0 else "N{}".format(format_num(i_5, 3))
+                    j_5 = "W{}".format(format_num(j_5, 3)) if j_5 < 0 else "E{}".format(format_num(j_5, 3))
+                    lon_lat = "({}° ~ {}° , {}° ~ {}°)".format(j_0[1:], j_5[1:], i_0[1:], i_5[1:])
+                    name = "{}{}_{}{}.tar.gz".format(i_0, j_0, i_5, j_5)
+                    url = alos_url_header + name
+                    download_url.append(url)
+                    html.append("{} <a href={}>{}</a>".format(lon_lat, url, name))
         # 每次点击获取链接按钮后，清空内容
         self.ted_info.clear()
+        self.ted_info.setFontUnderline(False)
+        self.ted_info.setTextColor(QColor('black'))
         # 添加一个开始锚点
         self.ted_info.append("<a name='begin'></a>")
         self.ted_info.insertPlainText(
             datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "\n\n获取到 " + str(
-                len(srtm_url)) + " 个DEM下载链接（点击文件名即可使用默认浏览器下载DEM）")
+                len(download_url)) + " 个DEM下载链接（点击文件名即可使用默认浏览器下载DEM）")
         self.ted_info.append("\n")
         # 插入可用的dem链接
         for i in range(len(html)):
             self.ted_info.insertHtml(str(i + 1) + "：" + html[i])
             self.ted_info.append('\n')
-        # 插入不可用的dem信息
+        # 插入不可用的dem信息(适用于SRTM)
         self.ted_info.setFontUnderline(False)
         self.ted_info.setTextColor(QColor('black'))
         for j in range(len(no_srtm)):
             self.ted_info.insertPlainText(str(len(html) + j + 1) + "：" + no_srtm[j] + "\n\n")
         # 滚动到锚点
         self.ted_info.scrollToAnchor('begin')
-        self.idm_thread.url = srtm_url
-        self.exec_thread.url = srtm_url
+        self.idm_thread.url = download_url
+        self.exec_thread.url = download_url
 
     def open_dir_slot(self):
         """打开对话框，选择DEM保存路径"""
